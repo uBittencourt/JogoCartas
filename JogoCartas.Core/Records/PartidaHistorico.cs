be000e6a3987d319471c99ee0591f6c56870f8ff
Vinicius <viniciusbittencourt113@gmail.com>
2026-03-24 09:39:22 -0300

    Criando estruturas para guardar histórico

---
+++ JogoCartas.Core/Records/PartidaHistorico.cs
@@ -0,0 +1,18 @@
+﻿using JogoCartas.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoCartas.Core.Records
+{
+    public record PartidaHistorico(
+        Guid Id,
+        StatusPartida Status,
+        DateTime Inicio,
+        DateTime? Fim,
+        IReadOnlyList<JogadorHistorico> Jogadores,
+        IReadOnlyList<RodadaHistorico> Rodadas
+    );
+}
---
+++ JogoCartas.Core/Records/RodadaHistorico.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoCartas.Core.Records
+{
+    public readonly record struct RodadaHistorico(
+        Guid Id,
+        int Numero
+    );
+}
